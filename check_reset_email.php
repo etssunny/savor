986a94749d3052c465223199a9e0500e49c5a1de
etssunny <43448887+etssunny@users.noreply.github.com>
2018-09-28 19:42:39 +0530

    Notified to send mail

---
+++ check_reset_email.php
@@ -0,0 +1,18 @@
+<?php  
+include 'header.php';
+?> 
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-6 col-sm-offset-3 col-xs-12">
+                <div class="welcome-sec">
+                    <h1>Welcome </h1> 
+ 						<label><h4>Email with password reset link has been sent to you. You may reset your password by clicking link sent to your Email Address</h4></label>
+                        
+                    <div class="last-btn">
+                        <a href="login.php"><p><button class="btn btn-primary btn-custom" type="login" name="login">Login Now</button></p></a>  
+                    </div>
+                </div> 
+            </div>
+        </div>
+    </div> 
+ 
